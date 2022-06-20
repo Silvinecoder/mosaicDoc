4a3b184bd4fc7ba101b710cd0305fa9af2a562d1
Cristiana-Simoes <cristiana.simoes@versantus.co.uk>
2022-06-20 21:30:16 +0100

    adding more notes to site settings, and a search input field

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,6 +5,28 @@
       <q-scroll-area
         style="height: calc(100% - 150px); border-right: 1px solid #ddd"
       >
+        <!--Search Bar-->
+        <q-input
+          square
+          dense
+          standout
+          v-model="textSearch"
+          v-on:keyup.enter="onEnter"
+          input-class="text-right"
+          label="not ready yet"
+        >
+          <template v-slot:append>
+            <q-icon v-if="textSearch === ''" name="search" />
+            <q-icon
+              v-else
+              name="clear"
+              class="cursor-pointer"
+              @click="textSearch = ''"
+            />
+          </template>
+        </q-input>
+
+        <!--Links-->
         <q-list padding>
           <q-item clickable v-ripple to="/sitesettings">
             <q-item-section> Site Settings </q-item-section>
@@ -24,17 +46,35 @@
 
 <script lang="ts">
 import { ref } from 'vue';
+import { defineComponent } from 'vue';
 
-export default {
-  setup() {
+export default defineComponent({
+  data() {
     const leftDrawerOpen = ref(false);
-
     return {
+      wordArray: '',
+      textSearch: '',
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
       },
     };
   },
-};
+
+  methods: {
+    onEnter: function () {
+      // On enter key
+      console.log('enter Key has been pressed', this.textSearch);
+    },
+
+    searchFilter() {
+      const search = this.textSearch;
+      if (search === '') {
+        // If search is empty
+        console.log('No text!');
+        return this.wordArray;
+      }
+    },
+  },
+});
 </script>
