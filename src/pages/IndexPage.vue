2cc0d4fcebb00c76e5527c52f0419c9e85a72261
Cristiana-Simoes <cristiana.simoes@versantus.co.uk>
2022-06-20 11:35:59 +0100

    removing index page

--- src/pages/IndexPage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <q-page class="row items-center justify-evenly"> Quasar </q-page>
-</template>
-
-<script lang="ts"></script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,6 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
       {
         path: '/sitesettings',
         component: () => import('pages/SiteSettings.vue'),
